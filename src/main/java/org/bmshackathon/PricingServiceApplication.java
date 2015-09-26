aafe0781af602162b8bf10852b4496e0aacb6ca9
p-bms <p-bms@users.noreply.github.com>
2015-09-26 13:54:29 +0200

    Added Spring Cloud Sleuth

--- src/main/java/org/bmshackathon/PricingServiceApplication.java
+++ src/main/java/org/bmshackathon/PricingServiceApplication.java
@@ -5,12 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix
 public class PricingServiceApplication {
+
+    @Bean
+    public Sampler<?> defaultSampler() {
+        return new AlwaysSampler();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PricingServiceApplication.class, args);
